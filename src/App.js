2e612b578a192e2188822a7569d038641be2b659
gefgu <gefgu@hotmail.com>
2022-07-20 09:54:23 -0300

    Remove positioning from styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import HomePage from "./components/HomePage";
 import theme from "./styled-components/theme";
-import Title from "./styled-components/Title";
 
 function App() {
   return (
--- src/styled-components/Title.js
+++ src/styled-components/Title.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 4rem;
-  text-align: center;
-  margin: 0 0;
   color: ${(props) => props.theme.primaryColor};
   font-family: ${(props) => props.theme.primaryFont};
 `;
