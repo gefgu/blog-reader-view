329d9d12523691cda5db4a452c7c9c34f603bdbd
gefgu <gefgu@hotmail.com>
2022-07-20 14:52:13 -0300

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import HomePage from "./components/HomePage";
 import theme from "./styled-components/theme";
@@ -5,7 +6,11 @@ import theme from "./styled-components/theme";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
