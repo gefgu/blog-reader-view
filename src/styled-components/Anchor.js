4a79617cfff1665398f7c49ccfcde4c0bd2c826f
gefgu <gefgu@hotmail.com>
2022-07-20 16:38:26 -0300

    Add Increase in size of link in navbar

--- src/styled-components/Anchor.js
+++ src/styled-components/Anchor.js
@@ -4,6 +4,7 @@ const Anchor = styled.a`
   color: ${(props) => props.theme.primaryColor};
   font-family: ${(props) => props.theme.primaryFont};
   text-decoration: none;
+  font-size: 1.25rem;
 `;
 
 export default Anchor;
