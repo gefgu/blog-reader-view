94e091536afc913a02929f15096b9c8836375390
gefgu <gefgu@hotmail.com>
2022-07-21 14:31:39 -0300

    Remove html tags from post preview

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -4,15 +4,15 @@ import Paragraph from "../styled-components/Paragraph";
 import SubHeading from "../styled-components/SubHeading";
 
 function PostPreview({ post }) {
+  post.content = post.content.replaceAll(/<.*?>/g, "");
+
   return (
     <ContentContainer>
       <Heading>{post.title}</Heading>
       <SubHeading>
         By {post.author} on {post.date}
       </SubHeading>
-      <Paragraph>
-        {post.content.slice(0, 200)}
-      </Paragraph>
+      <Paragraph>{post.content.slice(0, 200)}</Paragraph>
     </ContentContainer>
   );
 }
