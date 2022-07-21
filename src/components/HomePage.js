9247e75c7408a52b3c6283b18efe087a13cab6d3
gefgu <gefgu@hotmail.com>
2022-07-21 09:57:03 -0300

    Add fetching of post data

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ContentContainer from "../styled-components/ContentContainer";
 import Divider from "../styled-components/Divider";
 import Header from "../styled-components/Header";
@@ -7,7 +8,9 @@ import Title from "../styled-components/Title";
 import PostPreview from "./PostPreview";
 
 function HomePage() {
-  const posts = [
+  console.log(process.env.REACT_APP_API_URL);
+
+  const [posts, setPosts] = useState([
     {
       title: "Welcome to Blog",
       author: "Corban",
@@ -29,7 +32,17 @@ function HomePage() {
       content:
         "Welcome, it's great to have you here. We know that first impressions are important, so we've populated your new site with some initial getting started posts that will help you get familiar with everything in no time…",
     },
-  ];
+  ]);
+
+  const getPosts = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/posts`);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getPosts();
+  });
 
   return (
     <OuterWrapper>
@@ -39,7 +52,7 @@ function HomePage() {
       </Header>
       <ContentContainer>
         <Divider />
-        {posts.map((post) => {
+        {posts.map((post, index) => {
           return (
             <>
               <PostPreview post={post} />
