84ac6cb781f11fe539f828babe3963b50d95555d
gefgu <gefgu@hotmail.com>
2022-07-22 10:31:49 -0300

    Add delete button in comment box

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,5 @@
 import Box from "../styled-components/Box";
+import Button from "../styled-components/Button";
 import ContentContainer from "../styled-components/ContentContainer";
 import Paragraph from "../styled-components/Paragraph";
 
@@ -7,7 +8,10 @@ function CommentBox({ comment }) {
     <ContentContainer>
       <Box>
         <Paragraph>{comment.content}</Paragraph>
-        <Paragraph align="end" size="0.9rem">By {comment.author} on {comment.date}</Paragraph>
+        <Button>Delete</Button>
+        <Paragraph align="end" size="0.9rem">
+          By {comment.author} on {comment.date}
+        </Paragraph>
       </Box>
     </ContentContainer>
   );
--- src/styled-components/Box.js
+++ src/styled-components/Box.js
@@ -1,9 +1,15 @@
 import styled from "styled-components";
+import Button from "./Button";
 
 const Box = styled.div`
   border: 1px solid ${(props) => props.theme.secondaryColor};
   border-radius: 0.25rem;
   padding: 12px;
+
+  ${Button} {
+    display: block;
+    margin-left: auto;
+  }
 `;
 
 export default Box;
