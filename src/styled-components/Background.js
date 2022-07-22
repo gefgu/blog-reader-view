344a2073fdaa37d60f9f6d73d0cc0fbf9afb2591
gefgu <gefgu@hotmail.com>
2022-07-22 15:44:56 -0300

    Add min height for background

--- src/styled-components/Background.js
+++ src/styled-components/Background.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Background = styled.div`
   background-color: ${(props) => props.theme.backgroundColor};
+  min-height: 100vh;
 `;
 
 export default Background;
